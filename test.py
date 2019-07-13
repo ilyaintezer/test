37ce11e2fe765ca45c67bc9d06332b77b608daf1
ilyaintezer <ilya@intezer.com>
2019-07-13 16:21:51 +0300

    More testing

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+import redis
+import requests
+
+
+print(requests.get('http://google.com').content)
+
 print("hello")
+r = redis.Redis(host='localhost', port=6379, db=0,charset="utf-8", decode_responses=True)
+
+r.set('foo', 'bar')
+
+print(r.rpush('foo',ds))
